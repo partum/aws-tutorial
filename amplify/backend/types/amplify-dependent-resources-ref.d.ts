2a0537df94ebc8871fc46368e7fd3467cb77d8bd
Julia Arrington <jarringt@uccs.edu>
2022-12-05 09:41:24 -0700

    step 5 of tutorial

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s383b8ea9f": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
